bd729824ed30c6af701a92fe1ac0e4c01d2cca3e
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-26 09:04:54 -0300

    Class 370 - Finishing the Store Hook

--- src/hook-store/store.ts
+++ src/hook-store/store.ts
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
-const globalState = {};
+let globalState = {};
 let listeners: React.Dispatch<React.SetStateAction<typeof globalState>>[] = [];
-const actions = {};
+let actions: any = {};
 
-const useStore = () => {
+export const useStore = (): [
+  typeof globalState,
+  (actionIdentifier: string) => void
+] => {
   const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier: string) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
   useEffect(() => {
     listeners.push(setState);
@@ -14,4 +26,13 @@ const useStore = () => {
       listeners = listeners.filter((li) => li !== setState);
     };
   }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions: any, initialState: any): void => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
