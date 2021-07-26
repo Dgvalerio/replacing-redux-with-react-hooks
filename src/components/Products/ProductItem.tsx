42c3aa45b5c3d49ad85cfc41ac24a47e504085de
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-26 10:29:31 -0300

    Class 374 - Optimizing the Custom Hook Store

--- src/components/Products/ProductItem.tsx
+++ src/components/Products/ProductItem.tsx
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import { useStore } from '../../hook-store/store';
 import { IProduct } from '../../types/interfaces';
 import Card from '../UI/Card';
 import './ProductItem.css';
 
-const ProductItem = ({
-  id,
-  isFavorite,
-  description,
-  title,
-}: IProduct): JSX.Element => {
-  const dispatch = useStore()[1];
+const ProductItem = memo(
+  ({ id, isFavorite, description, title }: IProduct): JSX.Element => {
+    const dispatch = useStore(false)[1];
 
-  const toggleFavHandler = () => dispatch('toggleFav', id);
+    const toggleFavHandler = () => dispatch('toggleFav', id);
 
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={isFavorite ? 'is-fav' : ''}>{title}</h2>
-        <p>{description}</p>
-        <button
-          type="button"
-          className={!isFavorite ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {isFavorite ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
-};
+    return (
+      <Card style={{ marginBottom: '1rem' }}>
+        <div className="product-item">
+          <h2 className={isFavorite ? 'is-fav' : ''}>{title}</h2>
+          <p>{description}</p>
+          <button
+            type="button"
+            className={!isFavorite ? 'button-outline' : ''}
+            onClick={toggleFavHandler}
+          >
+            {isFavorite ? 'Un-Favorite' : 'Favorite'}
+          </button>
+        </div>
+      </Card>
+    );
+  }
+);
 
 export default ProductItem;
--- src/hook-store/store.ts
+++ src/hook-store/store.ts
@@ -6,10 +6,9 @@ let globalState: IProductsState | any = {};
 let listeners: React.Dispatch<React.SetStateAction<typeof globalState>>[] = [];
 let actions: any = {};
 
-export const useStore = (): [
-  typeof globalState,
-  (actionIdentifier: string, payload: any) => void
-] => {
+export const useStore = (
+  shouldListen = true
+): [typeof globalState, (actionIdentifier: string, payload: any) => void] => {
   const setState = useState(globalState)[1];
 
   const dispatch = (actionIdentifier: string, payload: any) => {
@@ -22,12 +21,16 @@ export const useStore = (): [
   };
 
   useEffect(() => {
-    listeners.push(setState);
+    if (shouldListen) {
+      listeners.push(setState);
+    }
 
     return () => {
-      listeners = listeners.filter((li) => li !== setState);
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
     };
-  }, [setState]);
+  }, [setState, shouldListen]);
 
   return [globalState, dispatch];
 };
